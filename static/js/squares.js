2f51d7f74fa72febdf29d40bf5ffbf73952e6300
Isaac Gonzalez <isaac@overttone.com>
2013-09-14 18:16:50 -0700

    work in progress

--- static/js/squares.js
+++ static/js/squares.js
@@ -1,50 +1,58 @@
+var w = new WebSocket("ws://127.0.0.1:9000/ws");
+
 var LOGGING = false;
 
-// ARGUMENTS: mouse x, mouse y, element x, element y, radius
-// send the x, y coordinates relative to the top left corner of
-// the objects bounding box.
-var relPosition = function(mX, mY, ex, ey, r) {
-    var topLeft = {x: ex - r, y: ey - r};
-    return {x: mX - topLeft.x, y: mY - topLeft.y};
-};
-
-// detect the mouse collision in each instrument in INSTRUMENTS
-var squareDetect = function(event) {
-    var _x = event.offsetX;
-    var _y = event.offsetY;
-    var instrument = undefined;
-    // iterate over each shape
-    for (i in INSTRUMENTS) {
-        instrument = INSTRUMENTS[i];
-        // detect which type of shape this is
-        if (instruments.type == 'rect') {
 
 
-        };
-    }
-};
-
-
-function sendCoordsOSC(event) {
-
-};
 
 var paper = Raphael(0, 0, "100%", "100%");
-paper.canvas.onmousemove = cDetect;
+paper.canvas.onmousemove = function (event) {
+    console.log('mx my', event.pageX, event.pageY);
+};
 
 var INSTRUMENTS = [];
 
 // creates a square; adds Raphael specific attributes to the raphael element
 var SQUARE_ID = 10;
-function createSquare(radius) {
-    var c1 = 100;
-    var c2 = 100;
-    var rElem = paper.rect(10, 10, 50, 50).attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5});;
+var c1 = 100;
+var c2 = 100;
+
+
+function createSquare(length, angle) {
+function getRelCoords(event) {
+    mx = event.pageX;
+    my = event.pageY;
+    console.log('mx', mx);
+    console.log('my', my);
+    console.log('angle', event.target.data('angle'));
+
+    ex = event.target.getBBox(true).x;
+    ey = event.target.getBBox(true).y;
+    console.log('ex', ex);
+    console.log('ey', ey);
+
+
+    // var r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    // var ang = Math.atan2(dy,dx);
+    // ang = ang - this.angle * (Math.PI / 180);
+
+    // dx = r * Math.cos(ang);
+    // dy = r * Math.sin(ang);
+
+    // var att = this.type == "rect" ? { x: this.ox + dx, y: this.oy + dy} : { cx: this.ox + dx, cy: this.oy + dy };
+    // this.attr(att);
+};
+    if (angle == undefined) {
+        angle = 0;
+    };
+    var rElem = paper.rect(c1, c2, length, length).attr({fill: "hsb(0, 1, 1)", stroke: "none", opacity: .5});;
     rElem.attr("fill", "#504");
 
     rElem.node.id = "" + SQUARE_ID;
     SQUARE_ID += 1;
 
+    rElem.data('angle', angle);
+    rElem.transform('r' + angle);
     var start = function () {
         this.x = this.attr("x");
         this.y = this.attr("y");
@@ -52,35 +60,56 @@ function createSquare(radius) {
     };
 
     var move = function (dx, dy) {
+        console.log('moving');
         this.attr({x: this.x + dx, y: this.y + dy});
     };
 
     var up = function () {
-        this.animate({opacity: .5}, 500, ">");
-        this.center = {x: this.data("x"), y: this.data("y")};
     };
+
+    // var start = function () {
+    //     this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
+    //     this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
+    // };
+
+    // var move = function (dx, dy) {
+    //     var r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    //     var ang = Math.atan2(dy,dx);
+    //     ang = ang - this.angle * (Math.PI / 180);
+
+    //     dx = r * Math.cos(ang);
+    //     dy = r * Math.sin(ang);
+
+    //     var att = this.type == "rect" ? { x: this.ox + dx, y: this.oy + dy} : { cx: this.ox + dx, cy: this.oy + dy };
+    //     this.attr(att);
+    // };
+
+    // var up = function () {
+    // };
+
 
     paper.set(rElem).drag(move, start, up);
 
     c1 += 40;
     c2 += 40;
 
-    rElem.mousemove(function (event){console.log(event.target);});
+    // rElem.mousemove(function (event){console.log(event.target);});
+    rElem.mousemove(getRelCoords);
+    rElem.click(whereIsXY);
+
     return rElem;
 };
 
-
+function whereIsXY(event) {
+    console.log('event', event.target);
+};
 
 var is = [];
 // create an arbitrary number of circles for this demo
 var START = function () {
     if (w.readyState == 1) {
         for (var i=0; i < 8; i++) {
-            var e = createSquare(100);
-            INSTRUMENTS.push(e);
-            if (w.readyState == 1){
-                chuckMakeInst("SinOsc", e.node.id);
-            };
+            createSquare(100);
         }
     }
 };
